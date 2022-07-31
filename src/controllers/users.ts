9bb2dbe01ac655e67273d68a160cd3741c902d48
Kain Nhantumbo <nhantumbok@gmail.com>
2022-07-31 16:29:16 +0200

    feat: added users controller.

---
+++ src/controllers/users.ts
@@ -0,0 +1,30 @@
+import UserModel from '../models/User';
+import BaseError from '../error/base-error';
+import { ControllerResponse } from '../types/functions';
+import { Request, Response } from 'express';
+
+export const getUsers = async (
+	req: Request,
+	res: Response
+): ControllerResponse => {
+	const users = await UserModel.find({});
+	res.status(200).json({ amount: users.length, users });
+};
+
+export const getUser = async (
+	req: Request,
+	res: Response
+): ControllerResponse => {
+	const { user_id } = req.body;
+	const user = await UserModel.findOne({ _id: user_id });
+	res.status(200).json({ user });
+};
+
+export const deleteUser = async (
+	req: Request,
+	res: Response
+): ControllerResponse => {
+	const { user_id } = req.body;
+	await UserModel.deleteOne({ _id: user_id });
+	res.status(200).json({ message: 'User deleted.' });
+};
