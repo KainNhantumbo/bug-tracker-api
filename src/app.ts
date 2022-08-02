a3d5bf503d8ae6ad284271399b9ec2e6b900189a
Kain Nhantumbo <nhantumbok@gmail.com>
2022-08-02 11:40:43 +0200

    feat: added bug routes.

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,16 @@ import bootstrap from './server/server';
 import { config } from 'dotenv';
 import globalErrorHandler from './error/global-error-handler';
 import { authRoutes } from './routes/auth';
+import { userRoutes } from './routes/users';
+import { bugRoutes } from './routes/bugs';
 
 config();
 const PORT = process.env.PORT || 6700;
 const app: Application = express();
 
 app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/bugs', bugRoutes);
 
 app.use(globalErrorHandler);
 
---
+++ src/routes/bugs.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import asyncWrapper from '../middleware/async-wrapper';
+import {
+	getSingleBug,
+	getAllBugs,
+	createBug,
+	deleteBug,
+	updateBug,
+} from '../controllers/bugs';
+
+const router = Router();
+
+router.route('/').get(asyncWrapper(getAllBugs)).post(asyncWrapper(createBug));
+
+router
+	.route('/:id')
+	.get(asyncWrapper(getSingleBug))
+	.patch(asyncWrapper(updateBug))
+	.delete(asyncWrapper(deleteBug));
+
+export { router as bugRoutes };
